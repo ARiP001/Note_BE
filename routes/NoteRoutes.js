d8217dd8894b90ca5a7c68aa339f4e586d5df614
ArifUPNYOG <123220136@student.upnyk.ac.id>
2025-02-20 13:16:32 +0700

    update

--- routes/NoteRoutes.js
+++ routes/NoteRoutes.js
@@ -2,7 +2,7 @@ import express from "express"
 import { getNotes, createNote, deleteNote, getNote, updateNote, getNoteByName } from "../controller/NoteController.js"
 
 const router = express.Router()
-
+kontol
 router.get('/notes', getNotes);
 router.post('/notes', createNote);
 router.delete('/notes/:id', deleteNote);
